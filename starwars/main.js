bde597d9c638d6091e50886bb02606d56ea706fc
Ashley Rice <ashleyrice@Ashleys-MacBook-Pro-2.local>
2020-03-12 12:03:25 -0600

    String Manipulation

---
+++ starwars/main.js
@@ -0,0 +1,67 @@
+import { films } from '../data/films.js'
+import { people } from '../data/people.js'
+
+const greetingDiv = document.querySelector('.greeting')
+const maleButton = document.querySelector('#maleButton')
+const femaleButton = document.querySelector('#femaleButton')
+const otherButton = document.querySelector('#otherButton')
+
+const otherCharacters = people.filter(person => {
+    if (
+    person.gender === "hermaphrodite" || 
+    person.gender === "n/a" || 
+    person.gender === "none"
+    ) {
+        return person
+    }
+})
+
+maleButton.addEventListener('click', event => {
+    populateDOM(people.filter(person => person.gender === "male"))
+})
+
+femaleButton.addEventListener('click', event => {
+    populateDOM(people.filter(person => person.gender === "female"))
+})
+
+otherButton.addEventListener('click', event => {
+    populateDOM(otherCharacters)
+})
+
+//"url": "https:swapi.co/api/people/1/"
+
+function getCharNumber(url) {
+    let end = url.lastIndexOf('/')
+    let start = end - 2
+    if(url.charAt(start) === '/') {
+        start++
+    }
+    return url.slice(start, end)
+}
+
+//getCharNumber("https:swapi.co/api/people/1/")
+
+function populateDOM(characters) {
+    characters.forEach(person => {
+     //need to extract the number from the person.url property
+     let charNum = getCharNumber(person.url)
+    let anchorWrap = document.createElement('a')
+anchorWrap.href = '#'
+
+let imageItem = document.createElement('img')
+imageItem.src = `https://starwars-visualguide.com/assets/img/characters/${charNum}.jpg`
+
+imageItem.addEventListener('error', event => {
+    //console.log(event)
+    imageItem.hidden = true
+    //imageItem.src = '../img/uvu.jpg'
+    })
+
+//add some way to handle user clicks on the image
+imageItem.addEventListener('click', event => {
+    console.log(event)
+})
+        anchorWrap.appendChild(imageItem)
+        greetingDiv.appendChild(anchorWrap)
+    })
+}
