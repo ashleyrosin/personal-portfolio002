e5a7fc759b715e13bb3aee80447fd5d7c9c70fc0
Ashley Rice <ashleyrice@Ashleys-MacBook-Pro-2.local>
2020-04-20 12:35:51 -0600

    added poke img

--- congress/index.js
+++ congress/index.js
@@ -1,14 +1,8 @@
 import { senators } from '../data/senators.js'
 
-// this is all about filter, map, reduce
+const senatorGrid = document.querySelector('.senatorGrid')
 
-const container = document.querySelector('.container')
-
-const filterSenators = (prop, value) => {
-    return senators.filter(senator => senator[prop] === value)
-}
-
-function simplifiedSenators(senatorArray) {
+function getSimplifiedSenators(senatorArray) {
     return senatorArray.map(senator => {
         let middleName = senator.middle_name ? ` ${senator.middle_name} ` : ` `
         return {
@@ -16,60 +10,93 @@ function simplifiedSenators(senatorArray) {
             name: `${senator.first_name}${middleName}${senator.last_name}`,
             imgURL: `https://www.govtrack.us/static/legislator-photos/${senator.govtrack_id}-200px.jpeg`,
             seniority: parseInt(senator.seniority, 10),
-            votesWithPartyPct: senator.votes_with_party_pct,
+            missedVotesPct: senator.missed_votes_pct,
             party: senator.party,
-            missed_votes_pct: senator.missed_votes_pct
+            loyaltyPct: senator.votes_with_party_pct
         }
     })
 }
 
-function populateContainer(smallSenatorsArray) {
-    return smallSenatorsArray.forEach(senator => {
-        
+function populateSenatorDiv(simpleSenators) {
+    console.log(simpleSenators)
+    simpleSenators.forEach(senator => {
+        let senDiv = document.createElement('div')
         let senFigure = document.createElement('figure')
         let figImg = document.createElement('img')
         let figCaption = document.createElement('figcaption')
-
+        let partyIcon = document.createElement('i')
+        if (senator.party === 'R') partyIcon.className = 'fas fa-republican'
+        if (senator.party === 'D') partyIcon.className = 'fas fa-democrat'
+        if (senator.party === 'ID') partyIcon.className = 'fas fa-star'
         figImg.src = senator.imgURL
         figCaption.textContent = senator.name
 
+        figCaption.appendChild(partyIcon)
         senFigure.appendChild(figImg)
         senFigure.appendChild(figCaption)
-        container.appendChild(senFigure)
+        senDiv.appendChild(senFigure)
+        senDiv.appendChild(progressBars(senator))
+        senatorGrid.appendChild(senDiv)
+    })
+}
+
+function progressBars(senator) {
+    let progressDiv = document.createElement('div')
+    progressDiv.className = 'progressDiv'
+    let loyaltyLabel = document.createElement('label')
+    loyaltyLabel.for = 'loyalty'
+    loyaltyLabel.textContent = 'Loyalty'
+    let loyaltyBar = document.createElement('progress')
+    loyaltyBar.id = 'loyalty'
+    loyaltyBar.max = 100
+    loyaltyBar.value = senator.loyaltyPct
+    let seniorityLabel = document.createElement('label')
+    seniorityLabel.for = 'seniority'
+    seniorityLabel.textContent = 'Seniority'
+    let seniorityBar = document.createElement('progress')
+    seniorityBar.id = 'seniority'
+    seniorityBar.max = 100
+    seniorityBar.value = parseInt((senator.seniority / mostSeniority.seniority) * 100)
+    let votingLabel = document.createElement('label')
+    votingLabel.for = 'voting'
+    votingLabel.textContent = 'Vote'
+    let votingBar = document.createElement('progress')
+    votingLabel.id = 'voting'
+    votingBar.max = 100
+    votingBar.value = 10
+
+    progressDiv.appendChild(loyaltyLabel)
+    progressDiv.appendChild(loyaltyBar)
+    progressDiv.appendChild(seniorityLabel)
+    progressDiv.appendChild(seniorityBar)
+    progressDiv.appendChild(votingLabel)
+    progressDiv.appendChild(votingBar)
+    return progressDiv
+}
+
+const filterSenators = (prop, value) => {
+    return senators.filter(senator => {
+        return senator[prop] === value
     })
 }
 
 const republicans = filterSenators('party', 'R')
 const democrats = filterSenators('party', 'D')
 
-const mostSeniority = simplifiedSenators(democrats).reduce(
-    (acc, senator) => {
-        return acc.seniority > senator.seniority ? acc : senator
-    }
-)
+const mostSeniority = getSimplifiedSenators(senators).reduce((acc, senator) => acc.seniority > senator.seniority ? acc : senator)
 
-const mostMissedVotes = simplifiedSenators(senators).reduce((acc, senator) => {
-    return acc.missed_votes_pct > senator.missed_votes_pct ? acc : senator
-}
-)
-
-/* const loyalArray = simplifiedSenators(senators).map(senator => {
-    if (senator.votesWithPartyPct === 100) {
-        return senator
-    }
-}) */
+const missedVotes = getSimplifiedSenators(senators).reduce((acc, senator) => acc.missedVotesPct > senator.missedVotesPct ? acc : senator)
 
 let loyalArray = []
 
- const mostLoyal = simplifiedSenators(senators).reduce((acc, senator) => {
-    if (senator.votesWithPartyPct === 100) {
+const mostLoyal = getSimplifiedSenators(republicans).reduce((acc, senator) => {
+    if (senator.loyaltyPct === 100) {
         loyalArray.push(senator)
     }
-    return acc.votesWithPartyPct > senator.votesWithPartyPct ? acc : senator
+    return acc.loyaltyPct > senator.loyaltyPct ? acc : senator
 })
 
-console.log(mostSeniority)
-console.log(loyalArray)
-console.log(mostMissedVotes)
+//console.log(loyalArray)
 
-populateContainer(simplifiedSenators(republicans))
+populateSenatorDiv(getSimplifiedSenators(senators))
+console.log(mostSeniority.seniority)
